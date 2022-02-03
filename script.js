a670c71308f3806efdd3080dd554827b8d9e18a5
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-02-03 16:02:06 -0300

    cartas aparecem na quantidade correta e viram ao clicar

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let numCartas = 0;
-function selecaoCartas(){
+function cardSelection(){
     numCartas = parseInt(prompt("Quantas cartas você deseja? (números de 4 a 14)"));
     while (numCartas > 14 || numCartas < 4 || numCartas%2 != 0){
         numCartas = parseInt(prompt("Quantas cartas você deseja? (números de 4 a 14)"));
     }
+    for(let i = 0; i < numCartas; i++){
+        let element = document.querySelector(".cards");
+        element.innerHTML += `
+        <div class="card" onclick="turn(this)">
+            <div class="front-face face">
+                Frente
+            </div>
+            <div class="back-face face">
+                Verso
+            </div>
+        </div>`
+    }
 }
 
-selecaoCartas();
+function turn(chosen){
+    let element = chosen.querySelector(".back-face");
+    element.classList.toggle("turn-back");
+    elementFront = chosen.querySelector(".face");
+    elementFront.classList.toggle("turn-front");
+}
+cardSelection();
