a77ca70464f0387952eba6833c83da880103805e
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-02-04 16:34:56 -0300

    cartas com gifs aleatorios

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 let numCartas = 0;
+let index = 0;
+const gifs = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"]
+const gifsShuffle = []
 function cardSelection(){
     numCartas = parseInt(prompt("Quantas cartas você deseja? (números de 4 a 14)"));
     while (numCartas > 14 || numCartas < 4 || numCartas%2 != 0){
         numCartas = parseInt(prompt("Quantas cartas você deseja? (números de 4 a 14)"));
     }
+    gifs.sort(comparador);
+    for(let z = 0; z < numCartas/2; z++){
+        gifsShuffle.push(gifs[z]);
+        gifsShuffle.push(gifs[z]);
+    }
+    gifsShuffle.sort(comparador);
     for(let i = 0; i < numCartas; i++){
         let element = document.querySelector(".cards");
         element.innerHTML += `
-        <div class="card" onclick="turn(this)">
+        <div class="card ${i}" onclick="turn(this)">
             <div class="front-face face">
                 Frente
             </div>
             <div class="back-face face">
-                Verso
+                <img src="${gifsShuffle[i]}"/>
             </div>
         </div>`
     }
 }
 
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
 function turn(chosen){
     let element = chosen.querySelector(".back-face");
-    element.classList.toggle("turn-back");
+    element.classList.add("turn-back");
     elementFront = chosen.querySelector(".face");
-    elementFront.classList.toggle("turn-front");
+    elementFront.classList.add("turn-front");
 }
+
 cardSelection();
