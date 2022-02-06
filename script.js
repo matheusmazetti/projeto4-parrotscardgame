f7aac872f6bd122d4de9bee39846d2e0796f6628
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-02-06 20:04:40 -0300

    jogo completo com bonus 2

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-let numCartas = 0;
+let numCards = 0;
 const selected = [];
 const gifs = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"]
 const gifsShuffle = []
 let firstSelected = null;
 let secondSelected = null;
+let plays = 0;
+let endCounter = 0;
 function cardSelection(){
-    numCartas = parseInt(prompt("Quantas cartas você deseja? (números de 4 a 14)"));
-    while (numCartas > 14 || numCartas < 4 || numCartas%2 != 0){
-        numCartas = parseInt(prompt("Quantas cartas você deseja? (números de 4 a 14)"));
+ numCards = parseInt(prompt("Quantas cartas você deseja? (números de 4 a 14)"));
+    while  (numCards > 14 || numCards < 4 || numCards%2 != 0){
+     numCards = parseInt(prompt("Quantas cartas você deseja? (números de 4 a 14)"));
     }
     gifs.sort(comparador);
-    for(let z = 0; z < numCartas/2; z++){
+    for(let z = 0; z < numCards/2; z++){
         gifsShuffle.push(gifs[z]);
         gifsShuffle.push(gifs[z]);
     }
     gifsShuffle.sort(comparador);
-    for(let i = 0; i < numCartas; i++){
+    for(let i = 0; i < numCards; i++){
         let element = document.querySelector(".cards");
         element.innerHTML += `
         <div class="card" onclick="turn(this)">
             <div class="front-face face">
-                Frente
+                <img src="front.png" />
             </div>
             <div class="back-face face">
                 <img src="${gifsShuffle[i]}"/>
@@ -40,6 +42,7 @@ function turn(chosen){
     elementFront.classList.add("turn-front");
     selected.push(chosen.querySelector(".back-face img").getAttribute("src"));
     chosen.setAttribute("onclick", "alert('Você já selecionou essa carta')");
+    plays++;
 
     if(firstSelected == null){
         firstSelected = chosen;
@@ -67,9 +70,13 @@ function turnBack(){
     secondElement.classList.remove("turn-front");
     firstSelected = null;
     secondSelected = null;
+    let disableClick = document.querySelector("body");
+    disableClick.classList.remove("disable-click");
 }
 
 function cardCompare(){
+    let disableClick = document.querySelector("body");
+    disableClick.classList.add("disable-click");
     let firstImg = firstSelected.querySelector(".back-face img");
     let secondImg = secondSelected.querySelector(".back-face img");
     if(firstImg.getAttribute("src") != secondImg.getAttribute("src")){
@@ -79,11 +86,27 @@ function cardCompare(){
         secondSelected.setAttribute("onclick", "alert('Você já descobriu este par')");
         firstSelected = null;
         secondSelected = null;
+        disableClick.classList.remove("disable-click");
+        endCounter++;
+        endGame();
     }
 }
 
 function deselect(){
     firstSelected = null;
     secondSelected = null;
+}
+
+function endGame(){
+    let restart = null;
+    if (endCounter == numCards/2){
+        alert(`Você ganhou em ${plays} jogadas!`);
+        restart = prompt("Gostaria de jogar novamente [s - sim/n - não]");
+    }
+    if (restart == "s"){
+        location.reload();
+    } else if(restart == "n"){
+        alert("Obrigado por jogar");4
+    }
 }
 cardSelection();
